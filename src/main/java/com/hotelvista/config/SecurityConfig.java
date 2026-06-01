b78231325777d3892b3c28aaaa5b839d5bdcb3b6
Hồ Quang Nhân <22715701.nhan@student.iuh.edu.vn>
2026-06-01 18:23:32 +0700

    feat: add internal

--- src/main/java/com/hotelvista/config/SecurityConfig.java
+++ src/main/java/com/hotelvista/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // allow preflight
+                .requestMatchers("/internal/**").permitAll()
                 .requestMatchers(HttpMethod.GET,
                     "/api/rooms",
                     "/api/rooms/**",
